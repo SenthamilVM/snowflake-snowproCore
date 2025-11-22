228fda864fc0e3d47035d7c17e1519d16c7a8c23
Senthamil <senthamilvm@gmail.com>
2025-11-22 23:51:26 +0530

    Table Stage

---
+++ 10.Table Stage.sql
@@ -0,0 +1,34 @@
+USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE LA_DB;
+USE SCHEMA LA_SCHEMA;
+
+--Create CUSTOMERS_USER table
+CREATE TABLE CUSTOMERS_TABLE 
+(
+CUSTOMER INT NOT NULL,
+FIRST_NAME VARCHAR(25) NOT NULL,
+LAST_NAME VARCHAR(25) NOT NULL,
+"REGION" VARCHAR(15) NOT NULL, --REGION IS A KEY WORD. Hence used ""
+EMAIL VARCHAR(50) NOT NULL,
+GENDER VARCHAR(10) NOT NULL,
+"ORDER" INT NOT NULL --ORDER IS A KEY WORD. Hence used ""
+);
+
+-- The following has to be executed using snowsql
+
+-- to list files in table stage
+list @%CUSTOMER_TABLE;
+
+
+PUT FILE://D:\Senthamil\Learning\Snowflake SnowPro Core Certification\Udemy Resources\Code+and+Data+Files\data\MOCK.csv @%/CUSTOMERS_TABLE/STAGED AUTO_COMPRESS = FALSE;
+
+PUT FILE://D:\Senthamil\Learning\Snowflake SnowPro Core Certification\Udemy Resources\Code+and+Data+Files\data\MOCK1.csv
+@%/CUSTOMERS_TABLE/STAGED AUTO_COMPRESS = FALSE;
+
+PUT FILE://D:\Senthamil\Learning\Snowflake SnowPro Core Certification\Udemy Resources\Code+and+Data+Files\data\MOCK1.csv
+@%/CUSTOMERS_TABLE/STAGED AUTO_COMPRESS = FALSE OVERWRITE = TRUE;
+
+COPY INTO CUSTOMERS_TABLE
+FROM @%CUSTOMERS_TABLE/STAGED
+FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1) PURGE = TRUE;
